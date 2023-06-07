94202b1d69f7c85c44c5cc273c2c49ba0a019c7d
elenatorneravalos <elenatorneravalos@hotmail.com>
2023-06-07 15:08:56 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -10,13 +10,14 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
+        raise ImportError(   
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment?"   
         ) from exc
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+# Call the main function to run administrative tasks      
+if __name__ == '__main__':   
     main()
